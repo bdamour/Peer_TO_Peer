e0d325c205ed5aa9729fe6e94ac4fbb36580fb8b
bdamour <benjamin.damour@insa-rennes.fr>
2016-11-29 22:30:59 +0100

    SERVER _ CLIENT _ STRUCTURE
    Dans ces 3 fichiers on a tous les morceaux de codes pour pouvoir faire
    la step1 (gestion des structures (encode et decode) envoit de chunk
    etc.)

--- Files/TEST_CLIENT_SERVER/Server.py
+++ Files/TEST_CLIENT_SERVER/Server.py
@@ -2,7 +2,8 @@
 import socket
 import threading
 import os
-import glob
+from Structure import *
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(("127.0.0.1", 1337))
@@ -10,12 +11,20 @@ sock.listen()
 
 def handle_client(client_sock, client_addr):
 	while True:
-		msg = client_sock.recv(1024).decode("utf-8")
-		if len(msg) == 0:
-			client_sock.close()
+		#On récupère la taille du chunck
+		msg_length = os.path.getsize("6f354416d03786f7480c2546c655f07e852c91bc.bin")
+		version = 1
+		msg_type = 48
+		print("version, msg_type,msg_length:\t", version, msg_type,msg_length)
+		binary = encode_header(version, msg_type,msg_length)
+		client_sock.send(binary)
+		fichier = open("6f354416d03786f7480c2546c655f07e852c91bc.bin","r+b")
+		chunck_content=fichier.read()
+		
+
+		client_sock.send(chunck_content)
 		break
-	print("%s:%d> %s" % (client_addr[0], client_addr[1], msg))
-	client_sock.send(msg[::-1].encode("utf-8"))
+	print("Envoyé !")
 
 
 while True:
@@ -23,6 +32,7 @@ while True:
 	th = threading.Thread(target=handle_client, args=(client_sock,client_addr))
 	th.daemon = True
 	th.start()
+
 
 os.system("pause")
 
--- Files/TEST_CLIENT_SERVER/Structure.py
+++ Files/TEST_CLIENT_SERVER/Structure.py
@@ -2,32 +2,97 @@
 import os
 import struct
 import glob
+import sys
 
-def encode(num, string):
-	string = string.encode("utf-8")
-	string_length = len(string)
-	fmt = "!HB%ds" % string_length
-	return struct.pack(fmt, num, string_length, string)
+def encode_header(version, msg_type,msg_length):
+	fmt = "!BB2xI"
+	return struct.pack(fmt, version,msg_type,msg_length)
 
-def decode(binary):
-	fmt1 = "!HB"
+def decode_header(binary):
+	fmt1 = "!BB2xI"
+	version, msg_type,msg_length = struct.unpack(fmt1, binary)
+	return (version, msg_type,msg_length)
+
+def encode_error(error_code):
+	fmt = "!H2x"
+	return struct.pack(fmt, error_code)
+
+def encode_filename(filename):
+	filename = filename.encode("utf-8")
+	lentgh_filename = len(filename)
+	lentgh = lentgh_filename
+	reste = lentgh%4
+	if reste==0:
+		lentgh += lentgh//4
+	else:
+		lentgh += reste
+	fmt = "!H%ds2x" % lentgh
+	return struct.pack(fmt,lentgh_filename, filename)
+
+def decode_filename(binary):
+	fmt1 = "!H"
 	fmt1_size = struct.calcsize(fmt1)
-	num, string_length = struct.unpack(fmt1, binary[:fmt1_size])
-	fmt2 = "!%ds" % string_length
-	(string,) = struct.unpack(fmt2, binary[fmt1_size:])
-	return (num, string.decode("utf-8"))
+	(filename_size,) =struct.unpack(fmt1, binary[:fmt1_size])
 
-num = 42
-string = "Hello World!"
-print("Original:\t", num, string)
+	fmt2 = "!%ds" % filename_size
+	fmt2_size = struct.calcsize(fmt2)
 
-binary = encode(num, string)
+	(filename,) = struct.unpack(fmt2, binary[fmt1_size:fmt2_size+fmt1_size])
+	return (filename_size,filename.decode("utf-8"))
+
+
+filename = "Bonjour toi petit escargot!"
+binary0 = encode_filename(filename)
+print(len(filename))
+print("Encoded:\t", " ".join(map(hex, bytes(binary0))))
+filesize,filename0 = decode_filename(binary0)
+print("Decoded:\t",filesize,filename0 )
+
+"""
+version = 1
+msg_type = 2
+msg_length = 1024
+print("Original:\t", version, msg_type,msg_length)
+binary = encode_header(version, msg_type,msg_length)
+
+print(binary[0:8]) #Selectionne les 8 premiers bites
+
 print("Encoded:\t", " ".join(map(hex, bytes(binary))))
 
-new_num, new_string = decode(binary)
-print("Decoded:\t", new_num, new_string)
+version2, msg_type2,msg_length2 = decode_header(binary)
+print("Decoded:\t", version2, msg_type2,msg_length2)
 
 
+
+# DISCOVER_TRACKER
+DISCOVER_TRACKER = encode_header(1,0,2)
+
+#GET_FILE_INFO
+GET_FILE_INFO = encode_header(1,2,2)
+
+
+#================ DECODE ENCODE CHUNCK_HASH 20 bytes ==============
+def encode(string):
+	string = string.encode("utf-8")
+	string_length = len(string)
+	fmt = "!20s" 
+	return struct.pack(fmt,string)
+
+def decode(binary):
+	fmt2 = "!20s" 
+	(string,) = struct.unpack(fmt2, binary)
+	return string.decode("utf-8")
+
+
+s = "7890e131622631d2e09e55a74ec7a1aab75d6330"
+print("Original:\t", s)
+binary0 = encode(s)
+print("Encoded:\t", " ".join(map(hex, bytes(binary0))))
+new_s = decode(binary0)
+print("Decoded:\t", new_s)
+"""
+
+"""
 #=======================================
 path = glob.glob("*.bin")
 print(path)
@@ -39,4 +104,5 @@ for i in range(size // 1024):
 
 #fichier.close()
 
-os.system("pause")
+
+os.system("pause")"""
